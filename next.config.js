1bf53b9bb74b93829ac5efafe2934631aaf3e195
shalini17827 <shalinichaudhary178@gmail.com>
2026-06-24 15:37:54 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "galaxy-prod.tlcdn.com",
+        pathname: "/**",
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
